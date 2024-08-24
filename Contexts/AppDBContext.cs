04e4f579a50bfb98e379e12db2418d0b50d39c25
VINIshow1907 <viniciuslucio792@gmail.com>
2024-08-24 17:07:17 -0300

    :sparkles: fiat: Criando a DTOs e adicionando seus recursos

--- Contexts/AppDBContext.cs
+++ Contexts/AppDBContext.cs
@@ -1,6 +1,26 @@
-﻿namespace ReserveiAPI.Contexts
+﻿using Microsoft.EntityFrameworkCore;
+using ReserveiAPI.Objects.Models.Entities;
+using ReserveiAPI.Contexts.Builders;
+
+namespace ReserveiAPI.Contexts
 {
-    public class AppDBContext
+    public class AppDBContext : DbContext
     {
+        //Mapeamento Relacional dos Objetos no Banco de Dados
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options){ }
+
+        //conjunto : Usuário
+
+        public DbSet<UserModel> Users { get; set; }
+
+        // Fluent API
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Entidades de Usuário:
+            UserBuilder.Build(modelBuilder);
+        }
     }
 }
--- Contexts/Builders/UserBuilder.cs
+++ Contexts/Builders/UserBuilder.cs
@@ -1,6 +1,33 @@
-﻿namespace ReserveiAPI.Contexts.Builders
+﻿
+using Microsoft.EntityFrameworkCore;
+using ReserveiAPI.Objects.Models.Entities;
+
+namespace ReserveiAPI.Contexts.Builders
 {
     public class UserBuilder
     {
+        public static void Build(ModelBuilder modelBuilder)
+        {
+            //Builder
+            modelBuilder.Entity<UserModel>().HasKey(u => u.Id);
+            modelBuilder.Entity<UserModel>().Property(u => u.ImageProfile);
+            modelBuilder.Entity<UserModel>().Property(u => u.NameUser).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<UserModel>().Property(u => u.EmailUser).HasMaxLength(200).IsRequired();
+            modelBuilder.Entity<UserModel>().Property(u => u.PasswordUser).HasMaxLength(256).IsRequired();
+            modelBuilder.Entity<UserModel>().Property(u => u.PhoneUser).HasMaxLength(15).IsRequired();
+
+            // Inserções
+            modelBuilder.Entity<UserModel>().HasData(
+                new UserModel
+                {
+                    Id = 1,
+                    NameUser = "Master",
+                    EmailUser = "master@development.com",
+                    PasswordUser = "ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f",
+                    PhoneUser = "",
+                    ImageProfile = ""
+                }
+            );
+        }
     }
 }
