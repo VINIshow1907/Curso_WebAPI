5afd3a200c54aad5c2868779ff0fd22cb98cd45e
VINIshow1907 <viniciuslucio792@gmail.com>
2024-08-25 21:10:26 -0300

    :sparkles: fiat: Criando a Services, Startup e foi configurada a program

---
+++ Services/Server/DependenciesInjection.cs
@@ -0,0 +1,18 @@
+﻿using ReserveiAPI.Repositories.Entities;
+using ReserveiAPI.Repositories.Interfaces;
+using ReserveiAPI.Services.Entities;
+using ReserveiAPI.Services.Interfaces;
+
+
+namespace ReserveiAPI.Services.Server
+{
+    public static class DependenciesInjection
+    {
+        public static void AdduserDependencies(this IServiceCollection services)
+        {
+            //Dependência: Usuário
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
+        }
+    }
+}
